6714aa81ffe6d32dcc9ec015a3047a2710c9e2d0
Amor <contact@amorkumar.com>
2020-08-05 23:39:59 -0400

    Refactor inspector controls

--- src/block/unsplash-random-image/controls.js
+++ src/block/unsplash-random-image/controls.js
@@ -2,19 +2,14 @@
  * CONTROLS: itsamoreh Unsplash Block
  */
 
-import { useEffect } from 'react';
-import { PanelBody, PanelRow, TextControl } from '@wordpress/components';
+import { Button, PanelBody, PanelRow, TextControl } from '@wordpress/components';
 import { InspectorControls } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 
 const Controls = ( props ) => {
 	const { attributes, setAttributes } = props;
-	const { accessKey, isSelected } = attributes;
-
-	useEffect( () => {
-		getAccessKey();
-	}, [ isSelected ] );
+	const { accessKey, isEditing = false } = attributes;
 
 	const createNotice = ( level, message ) => {
 		wp.data.dispatch( 'core/notices' ).createNotice(
@@ -24,38 +19,72 @@ const Controls = ( props ) => {
 		);
 	};
 
-	const getAccessKey = () => {
-		return apiFetch( {
-			path: '/akunsplashrandomimage/v1/access-key',
-			method: 'GET',
-		} )
-			.then( ( savedAccessKey ) => {
-				setAttributes( { accessKey: savedAccessKey } );
-			} )
-			.catch( ( error ) => createNotice( 'error', `${ __( 'There was an error retrieving a saved Unsplash API Access Key' ) }: "${ error.data.status } ${ error.message }"` ) );
+	const toggleEditMode = () => {
+		setAttributes( { isEditing: ! isEditing } );
 	};
 
-	const postAccessKey = () => {
-		return apiFetch( {
-			path: '/akunsplashrandomimage/v1/access-key',
-			method: 'POST',
-			body: accessKey,
-		} )
-			.catch( ( error ) => createNotice( 'error', `${ __( 'There was an error saving your Unsplash API Access Key' ) }: "${ error.data.status } ${ error.message }"` ) );
+	const getAccessKey = async () => {
+		try {
+			const savedAccessKey = await apiFetch( {
+				path: '/akunsplashrandomimage/v1/access-key',
+				method: 'GET',
+			} );
+			setAttributes( { accessKey: savedAccessKey } );
+		} catch ( error ) {
+			return createNotice( 'error', `${ __( 'There was an error retrieving a saved Unsplash API Access Key' ) }: "${ error.data.status } ${ error.message }"` );
+		}
+	};
+
+	const postAccessKey = async () => {
+		try {
+			return apiFetch( {
+				path: '/akunsplashrandomimage/v1/access-key',
+				method: 'POST',
+				body: accessKey,
+			} );
+		} catch ( error ) {
+			return createNotice( 'error', `${ __( 'There was an error saving your Unsplash API Access Key' ) }: "${ error.data.status } ${ error.message }"` );
+		}
 	};
 
 	return (
 		<InspectorControls>
 			<PanelBody>
-				<PanelRow>
-					<TextControl
-						label={ __( 'Unsplash API Access Key', 'unsplash-random-image' ) }
-						help={ 'https://unsplash.com/developers' }
-						value={ accessKey }
-						onChange={ ( newAccessKey ) => setAttributes( { accessKey: newAccessKey } ) }
-						onBlur={ postAccessKey }
-					/>
-				</PanelRow>
+				{
+					isEditing ? (
+						<>
+							<PanelRow>
+								<TextControl
+									label={ __( 'Unsplash API Access Key', 'unsplash-random-image' ) }
+									help={ 'https://unsplash.com/developers' }
+									value={ accessKey }
+									onChange={ ( newAccessKey ) => setAttributes( { accessKey: newAccessKey } ) }
+									onBlur={ postAccessKey }
+								/>
+							</PanelRow>
+							<PanelRow className="save-buttons">
+								<Button isSecondary onClick={ toggleEditMode }>{ __( 'Cancel' ) }</Button>
+								<Button isPrimary onClick={ () => {
+									postAccessKey();
+									toggleEditMode();
+								} }
+								>
+									{ __( 'Save Key' ) }
+								</Button>
+							</PanelRow>
+						</>
+					) : (
+						<PanelRow>
+							<Button isPrimary onClick={ () => {
+								getAccessKey();
+								toggleEditMode();
+							} }
+							>
+								{ __( 'Edit API Access Key' ) }
+							</Button>
+						</PanelRow>
+					)
+				}
 			</PanelBody>
 		</InspectorControls>
 	);
